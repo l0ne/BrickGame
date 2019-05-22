728d6d87cb21c8ed8b9716ae2808523050a42993
l0ne <a.c.judin@gmail.com>
2019-05-22 11:36:25 +0300

    some refactoring numbers

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,116 +1,45 @@
 import React from "react";
-import {View} from "react-native";
+import {StyleSheet, View} from "react-native";
 import {COLORS} from "../constants";
 
 export const Number = (props) => {
     const {number} = props;
-    let up = {}
-    let down = {}
+    let config = []; // left top rigth left top rigth bottom
     switch (number) {
-        case 1:
-            up = {borderRightColor: COLORS.black, borderTopColor: COLORS.grey, borderLeftColor: COLORS.grey};
-            down = {
-                borderRightColor: COLORS.black,
-                borderLeftColor: COLORS.grey,
-                borderTopColor: COLORS.grey,
-                borderBottomColor: COLORS.grey
-            };
-            break;
-        case 2:
-            up = {borderRightColor: COLORS.black, borderTopColor: COLORS.black, borderLeftColor: COLORS.grey};
-            down = {borderLeftColor: COLORS.black, borderTopColor: COLORS.black, borderBottomColor: COLORS.black, borderRightColor: COLORS.grey};
-            break;
-        case 3:
-            up = {borderRightColor: COLORS.black, borderTopColor: COLORS.black, borderLeftColor: COLORS.grey};
-            down = {borderRightColor: COLORS.black, borderTopColor: COLORS.black, borderBottomColor: COLORS.black, borderLeftColor: COLORS.grey};
-            break;
-        case 4:
-            up = {borderRightColor: COLORS.black, borderLeftColor: COLORS.black, borderTopColor: COLORS.grey};
-            down = {
-                borderRightColor: COLORS.black,
-                borderLeftColor: COLORS.grey,
-                borderTopColor: COLORS.black,
-                borderBottomColor: COLORS.grey
-            };
-            break;
-        case 5:
-            up = {
-                borderLeftColor: COLORS.black,
-                borderTopColor: COLORS.black,
-                borderRightColor: COLORS.grey
-            };
-            down = {
-                borderLeftColor: COLORS.grey,
-                borderRightColor: COLORS.black,
-                borderTopColor: COLORS.black,
-                borderBottomColor: COLORS.black
-            };
-            break;
-        case 6:
-            up = {borderLeftColor: COLORS.black, borderTopColor: COLORS.black, borderRightColor: COLORS.grey};
-            down = {
-                borderRightColor: COLORS.black,
-                borderTopColor: COLORS.black,
-                borderBottomColor: COLORS.black,
-                borderLeftColor: COLORS.black};
-            break;
-        case 7:
-            up = {borderRightColor: COLORS.black, borderTopColor: COLORS.black, borderLeftColor: COLORS.grey};
-            down = {
-                borderRightColor: COLORS.black,
-                borderLeftColor: COLORS.grey,
-                borderTopColor: COLORS.grey,
-                borderBottomColor: COLORS.grey
-            };
-            break;
-        case 8:
-            up = {borderRightColor: COLORS.black, borderTopColor: COLORS.black, borderLeftColor: COLORS.black};
-            down = {
-                borderRightColor: COLORS.black,
-                borderTopColor: COLORS.black,
-                borderLeftColor: COLORS.black,
-                borderBottomColor: COLORS.black
-            };
-            break;
-        case 9:
-            up = {
-                borderRightColor: COLORS.black,
-                borderTopColor: COLORS.black,
-                borderLeftColor: COLORS.black
-            };
-            down = {
-                borderRightColor: COLORS.black,
-                borderLeftColor: COLORS.grey,
-                borderTopColor: COLORS.black,
-                borderBottomColor: COLORS.black
-            };
-            break;
-        case 0:
-            up = {
-                borderTopColor: COLORS.black,
-                borderLeftColor: COLORS.black,
-                borderRightColor: COLORS.black,
-            };
-            down = {
-                borderTopColor: COLORS.grey,
-                borderLeftColor: COLORS.black,
-                borderRightColor: COLORS.black,
-                borderBottomColor: COLORS.black,
-            };
-            break;
-        default:
-            up = {
-                borderColor: COLORS.grey
-            };
-            down = {
-                borderColor: COLORS.grey
-            };
+        case 1: config = [0,0,1,0,0,1,0]; break;
+        case 2: config = [0,1,1,1,1,0,1]; break;
+        case 3: config = [0,1,1,0,1,1,1]; break;
+        case 4: config = [1,0,1,0,1,1,0]; break;
+        case 5: config = [1,1,0,0,1,1,1]; break;
+        case 6: config = [1,1,0,1,1,1,1]; break;
+        case 7: config = [0,1,1,0,0,1,0]; break;
+        case 8: config = [1,1,1,1,1,1,1]; break;
+        case 9: config = [1,1,1,0,1,1,1]; break;
+        case 0: config = [1,1,1,1,0,1,1]; break;
+        default: config = [0,0,0,0,0,0,0];
     }
+
+    let up = {
+        borderLeftColor: config[0] ? COLORS.black : COLORS.grey,
+        borderTopColor: config[1] ? COLORS.black : COLORS.grey,
+        borderRightColor: config[2] ? COLORS.black : COLORS.grey,
+    };
+    let down = {
+        borderLeftColor: config[3] ? COLORS.black : COLORS.grey,
+        borderTopColor: config[4] ? COLORS.black : COLORS.grey,
+        borderRightColor: config[5] ? COLORS.black : COLORS.grey,
+        borderBottomColor: config[6] ? COLORS.black : COLORS.grey,
+        marginTop: -1
+    };
 
     return (
         <View style={{transform: [{ rotate: '2deg'}], marginRight: 1, marginLeft: 1}}>
-            <View style={[{width: 7, height: 8, borderWidth: 1}, up]}/>
-            <View style={[{width: 7, height: 8, borderWidth: 1, marginTop: -1}, down]}/>
+            <View style={[styles.section, up]}/>
+            <View style={[styles.section, down]}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    section: {width: 7, height: 8, borderWidth: 1}
+});
